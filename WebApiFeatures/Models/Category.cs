9c86bc3fab838a5fc3ba2e8d8dc331cfdb594824
nishanthsuraj <nishanthsuraj@gmail.com>
2024-02-14 15:26:24 +0530

    Creating a data model

---
+++ WebApiFeatures/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiFeatures.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ WebApiFeatures/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApiFeatures.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+    }
+}
